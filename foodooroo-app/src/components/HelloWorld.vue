8d7e74f4bbae3d13ffcdc086e4d1eb0eff1eb6dd
Usman Balesaria <ubalesaria@me.com>
2018-06-07 15:03:20 +0100

    Added the weather pictures to appear when releavnt weather is forecasted

--- foodooroo-app/src/components/HelloWorld.vue
+++ foodooroo-app/src/components/HelloWorld.vue
@@ -1,13 +1,33 @@
 <template>
-  <div class="hello">
-    <input ref="autocomplete"
-           placeholder="Enter your address"
-           type="text"
-           class="column is-half"
-    />
-
-    <p v-if="this.weather.timezone">{{this.weather.timezone}}</p>
-    <p v-if="this.weather.timezone">{{ this.weather.currently.summary }}</p>
+  <div class="">
+    <h1 class="title is-1 has-text-centered"> FOODOO (to-the) ROO 🍔 🐓 🇬🇧 🇵🇱 🇮🇳 🇦🇺</h1>
+    <div class="hello container">
+      <input ref="autocomplete"
+             placeholder="Enter your address"
+             type="text"
+             class="input"
+      />
+      <div v-if="this.weather.timezone" class="columns">
+        <div class="column">
+          <h1 class="title is-2">Todays Weather</h1>
+          <p>{{this.weather.timezone}}</p>
+          <p>{{ this.weather.currently.summary }}</p>
+          <p> {{(parseFloat((this.weather.currently.temperature)-32)*5/9).toFixed(1)}} Degrees </p>
+        </div>
+          <div class="column" v-if="this.weather.timezone">
+            <h1 class="title is-2">Weather Forecast</h1>
+            <p > {{this.weather.daily.summary}} </p>
+          </div>
+      </div>
+      <div v-if="this.weather.timezone" class="columns is-centered">
+        <div class="column has-text-centered">
+          <img v-if="this.weather.currently.summary === 'Mostly Cloudy'" src="https://media.self.com/photos/5a625de37b39d91320a3ab0c/4:3/w_728,c_limit/cloudy-pee.jpg" />
+          <img v-if="this.weather.currently.summary === 'Overcast'" src="https://ak6.picdn.net/shutterstock/videos/2615726/thumb/1.jpg?i10c=img.resize(height:160)" />
+          <img v-if="this.weather.currently.summary === 'Sunny'" src="http://magarticles.magzter.com/articles/4212/195779/582eb9d4ed894/Sunny-Side-Trump.jpg" />
+          <img v-if="this.weather.currently.summary === 'Rainy'" src="http://thechronicleherald.ca/sites/default/files/imagecache/ch_article_main_image/articles/B97693421Z.120170609150556000GFHHF80P.11.jpg" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
